56cf4fe7351bb3211264f936b408ada872f85c1f
MellerPeller <melker.eriksson@skola.sala.se>
2024-09-11 10:26:11 +0200

    Add project files.

---
+++ BankAccount WinForms/BankAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount_WinForms
+{
+    public class BankAccount
+    {
+
+        int balance = 0;
+
+        public void Deposit(int amount)
+        {
+
+
+            balance += amount;
+
+        }
+
+        public void Withdraw(int amount)
+        {
+            if (balance > (amount -1))
+            {
+                balance -= amount;
+            }
+
+        }
+
+
+        public int GetBalance()
+        {
+
+            return balance;
+
+        }
+
+    }
+}
